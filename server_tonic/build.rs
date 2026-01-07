060b0118ec186c11f8cf1dcfc1914459af88358c
marulitua <erwinmaruli@live.com>
2026-01-07 10:42:21 +0800

    update hyper and tonic

--- server_tonic/build.rs
+++ server_tonic/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../android/protos/src/proto/com/hepeng/note/helloworld/helloworld.proto")?;
+    tonic_prost_build::compile_protos("../android/protos/src/proto/com/hepeng/note/helloworld/helloworld.proto")?;
     Ok(())
 }
